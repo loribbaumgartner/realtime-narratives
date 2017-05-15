1111454262849b7f860edf88fa621a9e62465179
Lori Baumgartner <lori@automatedinsights.com>
2017-05-15 17:05:19 -0400

    set up routes. page loading.

---
+++ config/initializers/pusher.rb
@@ -0,0 +1,8 @@
+require 'pusher'
+
+Pusher.app_id = '339635'
+Pusher.key = '0921d05e113bd5ac4866'
+Pusher.secret = '9ad35dcce1e02efda1a0'
+Pusher.cluster = 'us2'
+Pusher.logger = Rails.logger
+Pusher.encrypted = true
