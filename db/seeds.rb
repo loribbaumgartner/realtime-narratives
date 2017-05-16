8374d1635ed5a8b8e9043b81f9b42245201ffeb8
Lori Baumgartner <lori@automatedinsights.com>
2017-05-16 08:58:51 -0400

    fix timezones in db.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Narrative.create(narratives_generated: )
+start_time = Time.new(2017, 05, 16, 9, 00, 0)
+start_narratives = 134
+
+1000.times do
+  Narrative.create(narratives_generated: start_narratives, created_at: start_time)
+  start_time += 30.seconds
+  start_narratives += 345
+end
